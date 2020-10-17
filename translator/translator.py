eee1115f8711168ba31f3ef422bee3df7ca69c39
Vladimir <mr.katenin@mail.ru>
2020-10-17 20:54:37 +0300

    added 13 lang-s

--- translator/translator.py
+++ translator/translator.py
@@ -1,33 +1,45 @@
-import string
 import requests
 from bs4 import BeautifulSoup
 
+LANGUAGES = ("arabic",
+             "german",
+             "english",
+             "spanish",
+             "french",
+             "hebrew",
+             "japanese",
+             "dutch",
+             "polish",
+             "portuguese",
+             "romanian",
+             "russian",
+             "turkish",
+             )
+
 
 def choose_language():
-    lang = input('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:\n')
-    if lang == "fr":
-        return "english-french", "fr"
-    elif lang == "en":
-        return "french-english", "en"
+    for i in range(len(LANGUAGES)):
+        print(f"{i+1}. {LANGUAGES[i].title()}")
+    first_language_number = int(input("Type the number of your language:\n"))
+    second_language_number = int(input("Type the number of language you want to translate to:\n"))
+    return LANGUAGES[first_language_number-1], LANGUAGES[second_language_number-1]
 
 
 def main():
-    language, lang = choose_language()
-    print('Type the word you want to translate:')
-    word = input()
-    print(f'You chose "{language}" as a language to translate "{word}".')
+    print("Hello, you're welcome to the translator. Translator supports:")
+    first_language, second_language = choose_language()
+    word = input("Type the word you want to translate:\n")
+
     user_agent = 'Mozilla/5.0'
-    url = f"https://context.reverso.net/translation/{language}/{word}"
+    url = f"https://context.reverso.net/translation/{first_language}-{second_language}/{word}"
     response = requests.get(url, headers={'User-Agent': user_agent})
     soup = BeautifulSoup(response.content, "lxml")
 
-    if response.status_code == 200:
-        print(response.status_code, "OK")
-
-    print("\nContext examples:")
+    # if response.status_code == 200:
+    #     print(response.status_code, "OK")
 
     translations = []
-    print("\nFrench Translations:")
+    print(f"\n{second_language.title()} Translations:")
     for translation in soup.select('[class*="translation ltr dict"]'):
         translation = translation.get_text().strip()
         translation = ''.join([i if ord(i) < 128 else '?' for i in translation])
@@ -37,7 +49,7 @@ def main():
         print(translation)
 
     french_examples = []
-    print("\nFrench Examples:")
+    print(f"\n{second_language.title()} Examples:")
     for translation in soup.find_all('div', {'class': ['src ltr', 'trg ltr']}):
         translation = translation.get_text().strip()
         translation = ''.join([i if ord(i) < 128 else '?' for i in translation])
@@ -45,9 +57,6 @@ def main():
 
     for i in range(1, 10, 2):
         print(f"{french_examples[i-1]}:\n{french_examples[i]}\n")
-
-    #print(translations)
-    #print(sent_translations)
 
 
 if __name__ == "__main__":
